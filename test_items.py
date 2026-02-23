3ff57af45105d86034a3e1261c150f31d7c1ba57
Yaroslav <umpepatopChannel@gmail.com>
2026-02-24 01:13:50 +0500

    simplify test_items.py

--- test_items.py
+++ test_items.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -6,7 +7,6 @@ def test_add_to_basket_button_exists(browser):
     # Внимание! Сайт недоступен в России!
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button = WebDriverWait(browser, 5).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
-    )
+    time.sleep(5)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button is not None, "Кнопка не найдена"
